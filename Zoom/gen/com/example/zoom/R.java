4a4e96d86f12653ebb924642824cb129c055316d
miko023 <michael_chiang@hotmail.com>
2013-06-25 21:06:59 -0700

    working marker

--- Zoom/gen/com/example/zoom/R.java
+++ Zoom/gen/com/example/zoom/R.java
@@ -182,9 +182,11 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020018;
     }
     public static final class id {
-        public static final int action_settings=0x7f050008;
-        public static final int btnCamera=0x7f050006;
-        public static final int btnVancouver=0x7f050007;
+        public static final int Latitude=0x7f050006;
+        public static final int Longitude=0x7f050007;
+        public static final int action_settings=0x7f05000a;
+        public static final int btnCamera=0x7f050008;
+        public static final int btnVancouver=0x7f050009;
         public static final int hybrid=0x7f050004;
         public static final int map=0x7f050005;
         public static final int none=0x7f050000;
